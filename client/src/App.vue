81ea56923f9a0e7636462faee407e70f8027c1ca
kaiverx <praim200570@gmail.com>
2026-05-26 04:38:34 +0800

    админка

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,30 @@
 <template>
+  <nav>
+    <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Пользователь
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/admin">Админка</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <router-view />
 </template>
-
+ 
 <script setup>
 import { onBeforeMount } from 'vue';
 import axios from 'axios';
 import Cookies from 'js-cookie';
-
+ 
 onBeforeMount(() => {
   axios.defaults.headers.common['X-CSRFToken'] = Cookies.get("csrftoken");
 })
 </script>
+ 
